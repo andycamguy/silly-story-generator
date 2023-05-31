ea2fb4782b19920758a7eb5a42f3414f99c71ce5
Andrew Ross <134294344+andycamguy@users.noreply.github.com>
2023-05-31 14:49:01 -0400

    Thanks Brad Kerr for all the help

--- main.js
+++ main.js
@@ -10,51 +10,47 @@ function randomValueFromArray(array){
 
 //raw text settings
 var storyText = "It was 94 fahrenheit outside, so ${xItem} went for a walk. When they got to ${yItem}, they stared in horror for a few moments, then ${zItem}. Bob saw the whole thing, but was not surprised — ${xItem} weighs 300 pounds, and it was a hot day.";
-const insertX = 
-[ "Willy the Goblin",
-"Big Daddy",
-"Father Christmas" ]
-const insertY =[
-"the soup kitchen",
-"Disneyland",
-"the White House"]
-const insertZ =["spontaneously combusted",
-"melted into a puddle on the sidewalk",
-"turned into a slug and crawled away"]
+const insertX = [ "Willy the Goblin", "Big Daddy", "Father Christmas" ]
+const insertY =[ "the soup kitchen", "Disneyland", "the White House"]
+const insertZ =["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"]
 
 // EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
 
 function result() {
+  let newStory = storyText;
+  let xItem =randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+  newStory =.newStory.replace(':itemx:'insertX)
+  newStory =.newStory.replace(':itemy:'inserty)
+  newStory =.newStory.replace(':itemz:'insertZ)
+  newStory =.newStory.replace(':itemx:'insertX)
+
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory.name = customName.value;
+    newStory = newStory.replaceAll('Bob,name);
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300*0.0714286);
+    const weight = ${Math.round(300*0.0714286)}+"stone";
     const temperature =  Math.round((94 - 32) * 5/9);
+    newStory = newStory.replaceAll('300 pounds', weight);
+    newStory = newStory.replaceAll('94 fahrenheit, temperature);
 
   }
 
   story.textContent = newStory;
   story.style.visibility = 'visible';
 }
-let newStory = storyText;
-let xItem =randomValueFromArray(insertX);
-let yItem = randomValueFromArray(insertY);
-let zItem = randomValueFromArray(insertZ);
 
  
   story.textContent = NewStory;
   story.style.visibility = 'visible';
 }
-let newStory = storyText;
-let xItem =randomValueFromArray(insertX);
-let yItem = randomValueFromArray(insertY);
-let zItem = randomValueFromArray(insertZ);
+
 
 randomize.addEventListener('click', result);
 
