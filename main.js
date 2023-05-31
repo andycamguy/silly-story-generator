8b8e7c9237634dd9392a22175752bb37ddf7fbdd
Andrew Ross <134294344+andycamguy@users.noreply.github.com>
2023-05-31 12:28:20 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+//complete variable and function definitions
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
 
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
