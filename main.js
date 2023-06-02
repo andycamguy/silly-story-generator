276f146d9f378a9c0ddabb1c1ffb67a8bd9c3acf
Andrew Ross <134294344+andycamguy@users.noreply.github.com>
2023-06-02 00:30:38 -0400

    im ok i promise

--- main.js
+++ main.js
@@ -35,8 +35,8 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = '${Math.round(300*0.0714286)+"stone"}';
-    const temperature =  '${Math.round((94 - 32) * 5/9) + "celcius}';
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature =  `${Math.round((94 - 32) * 5/9)} Celsius`;
     newStory = newStory.replaceAll('300 pounds', weight);
     newStory = newStory.replaceAll('94 fahrenheit', temperature);
 
